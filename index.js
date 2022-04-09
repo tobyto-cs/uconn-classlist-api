42266261ca15eb2f1bef52f587af1e6614291691
Jeremy Goode <jeremygoode1029@gmail.com>
2022-04-09 15:49:11 -0400

    update index to function as node module

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const api = require("./api.js");
 
 // api.getClassList().then(() => { console.log("File downloaded"); });
 api.xlsxToJson();
+
+const xlsxToJson = () => {
+  return api.xlsxToJson();
+}
+
+module.exports = {
+  xlsxToJson
+}
